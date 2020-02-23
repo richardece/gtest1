f39813988796a3b2be001e5b0ea28f31dd276bd1
Richard <richard_ece2k5@yahoo.com>
2020-02-23 17:22:00 +0800

    Add token from codecov

--- code_cov.sh
+++ code_cov.sh
@@ -9,5 +9,5 @@ done
 
 lcov --capture --directory . --output-file coverage.info
 
-bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
+bash <(curl -s https://codecov.io/bash) -t b6718535-f583-4001-b742-d67ea7a33535 || echo "Codecov did not collect coverage reports"
 
